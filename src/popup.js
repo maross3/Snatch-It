1fd876e5b6b0db1bf9cd3f8dbd71a3515020a5e3
maross3 <maross334@gmail.com>
2022-10-25 21:14:43 -0400

    refactor

--- src/popup.js
+++ src/popup.js
@@ -2,8 +2,12 @@ import { Graph } from "./graph.js";
 import { makeTabs } from "./tabs_helper.js"
 
 // todo export JSON obj to cookies, parse and include here
-// get all active and inactive tabs
+
+
+// make tabs in popup
 const allTabs = await chrome.tabs.query({ });
 makeTabs(allTabs);
+
+
 
 
